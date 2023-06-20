f19e9b976f9e2e8f07480cb1e6aaa6bd492adba8
Robin Punnoose <robin.punnoose@protonmail.com>
2023-06-20 12:58:52 -0500

    added freshman first dapp

---
+++ learn-web3/freshman/first-dapp/contract.js
@@ -0,0 +1,62 @@
+const { ethers } = window;
+
+const MoodContractAddress = "0xAB32180EA08ccd7827409B29437afE841ed8F451";
+
+const MoodContractABI = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_mood",
+        type: "string",
+      },
+    ],
+    name: "setMood",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getMood",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+let MoodContract;
+
+let signer;
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "sepolia");
+
+provider.send("eth_requestAccounts", []).then(() => {
+  provider.listAccounts().then((accounts) => {
+    signer = provider.getSigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+      MoodContractAddress,
+      MoodContractABI,
+      signer
+    );
+  });
+});
+
+async function getMood() {
+  const getMoodPromise = MoodContract.getMood();
+  const Mood = await getMoodPromise;
+  document.getElementById("showMood").innerText = `Your Mood: ${Mood}`;
+  console.log(Mood);
+}
+
+async function setMood() {
+  const mood = document.getElementById("mood").value;
+  const setMoodPromise = MoodContract.setMood(mood);
+  await setMoodPromise;
+}
