4d758a78ee1fba2976c88ebb9a29ee93fd0ecff8
Robin Punnoose <robin.punnoose@protonmail.com>
2023-07-15 13:47:36 -0500

    added senior meta transactions

---
+++ learn-web3/04-senior/10-meta-transactions/test/metatxn-test.js
@@ -0,0 +1,81 @@
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { arrayify, parseEther } = require("ethers/lib/utils");
+const { ethers } = require("hardhat");
+
+describe("MetaTokenTransfer", function () {
+  it("Should let user transfer tokens through a relayer", async function () {
+    // Deploy the contracts
+    const RandomTokenFactory = await ethers.getContractFactory("RandomToken");
+    const randomTokenContract = await RandomTokenFactory.deploy();
+    await randomTokenContract.deployed();
+
+    const MetaTokenSenderFactory = await ethers.getContractFactory(
+      "TokenSender"
+    );
+    const tokenSenderContract = await MetaTokenSenderFactory.deploy();
+    await tokenSenderContract.deployed();
+
+    // Get three addresses, treat one as the user address
+    // one as the relayer address, and one as a recipient address
+    const [_, userAddress, relayerAddress, recipientAddress] =
+      await ethers.getSigners();
+
+    // Mint 10,000 tokens to user address (for testing)
+    const tenThousandTokensWithDecimals = parseEther("10000");
+    const userTokenContractInstance = randomTokenContract.connect(userAddress);
+    const mintTxn = await userTokenContractInstance.freeMint(
+      tenThousandTokensWithDecimals
+    );
+    await mintTxn.wait();
+
+    // Have user infinite approve the token sender contract for transferring 'RandomToken'
+    const approveTxn = await userTokenContractInstance.approve(
+      tokenSenderContract.address,
+      BigNumber.from(
+        // This is uint256's max value (2^256 - 1) in hex
+        // Fun Fact: There are 64 f's in here.
+        // In hexadecimal, each digit can represent 4 bits
+        // f is the largest digit in hexadecimal (1111 in binary)
+        // 4 + 4 = 8 i.e. two hex digits = 1 byte
+        // 64 digits = 32 bytes
+        // 32 bytes = 256 bits = uint256
+        "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+      )
+    );
+    await approveTxn.wait();
+
+    // Have user sign message to transfer 10 tokens to recipient
+    const transferAmountOfTokens = parseEther("10");
+    const messageHash = await tokenSenderContract.getHash(
+      userAddress.address,
+      transferAmountOfTokens,
+      recipientAddress.address,
+      randomTokenContract.address
+    );
+    const signature = await userAddress.signMessage(arrayify(messageHash));
+
+    // Have the relayer execute the transaction on behalf of the user
+    const relayerSenderContractInstance =
+      tokenSenderContract.connect(relayerAddress);
+    const metaTxn = await relayerSenderContractInstance.transfer(
+      userAddress.address,
+      transferAmountOfTokens,
+      recipientAddress.address,
+      randomTokenContract.address,
+      signature
+    );
+    await metaTxn.wait();
+
+    // Check the user's balance decreased, and recipient got 10 tokens
+    const userBalance = await randomTokenContract.balanceOf(
+      userAddress.address
+    );
+    const recipientBalance = await randomTokenContract.balanceOf(
+      recipientAddress.address
+    );
+
+    expect(userBalance.lt(tenThousandTokensWithDecimals)).to.be.true;
+    expect(recipientBalance.gt(BigNumber.from(0))).to.be.true;
+  });
+});
