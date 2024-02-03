e3c52fc7b7cf5229b0f26f5002d565b8a5fee025
Robin Punnoose <robin.punnoose@protonmail.com>
2024-02-03 09:30:11 -0600

    added tests for DSCEngineTest.t.sol

--- cyfrin-foundry-course/12-stable-coin/test/unit/DSCEngineTest.t.sol
+++ cyfrin-foundry-course/12-stable-coin/test/unit/DSCEngineTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
@@ -19,6 +19,7 @@ contract DSCEngineTest is Test {
 
     address public USER = makeAddr("user");
     uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant AMOUNT_TO_MINT = 100 ether;
     uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
     function setUp() public {
@@ -27,6 +28,25 @@ contract DSCEngineTest is Test {
         (ethUsdPriceFeed, btcUsdPriceFeed, weth,,) = config.activeNetworkConfig();
 
         ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
+
+    ///////////////
+    // modifiers //
+    ///////////////
+    modifier depositedCollateral() {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+        dsce.depositCollateral(weth, AMOUNT_COLLATERAL);
+        vm.stopPrank();
+        _;
+    }
+
+    modifier depositedCollateralAndMintDsc() {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+        dsce.depositCollateralAndMintDsc(weth, AMOUNT_COLLATERAL, AMOUNT_TO_MINT);
+        vm.stopPrank();
+        _;
     }
 
     ///////////////////////
@@ -81,14 +101,6 @@ contract DSCEngineTest is Test {
         vm.stopPrank();
     }
 
-    modifier depositedCollateral() {
-        vm.startPrank(USER);
-        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
-        dsce.depositCollateral(weth, AMOUNT_COLLATERAL);
-        vm.stopPrank();
-        _;
-    }
-
     function testCanDepositCollateralAndGetAccountInfo() public depositedCollateral {
         (uint256 totalDscMinted, uint256 collateralValueInUsd) = dsce.getAccountInformation(USER);
 
@@ -97,5 +109,87 @@ contract DSCEngineTest is Test {
 
         assertEq(totalDscMinted, expectedTotalDscMinted);
         assertEq(AMOUNT_COLLATERAL, expectedDepositAmount);
+    }
+
+    ///////////////////
+    // mintDsc Tests //
+    ///////////////////
+    function testRevertWithoutCollateral() public {
+        vm.startPrank(USER);
+        // bytes4 selector = bytes4(keccak256("DSCEngine__BreaksHealthFactor(uint256)"));
+        // vm.expectRevert(abi.encodeWithSelector(selector, type(uint256).max));
+        vm.expectRevert();
+        dsce.mintDsc(AMOUNT_COLLATERAL / 10);
+        vm.stopPrank();
+    }
+
+    function testReverIfMintIsZero() public depositedCollateral {
+        vm.startPrank(USER);
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.mintDsc(0);
+        vm.stopPrank();
+    }
+
+    function testBreakHealthFactor() public depositedCollateral {
+        vm.startPrank(USER);
+        dsce.mintDsc(AMOUNT_TO_MINT);
+        // bytes4 selector = bytes4(keccak256("DSCEngine__BreaksHealthFactor(uint256)"));
+        // uint256 healthFactor = dsce.getHealthFactor(USER);
+        // vm.expectRevert(abi.encodeWithSelector(selector, healthFactor));
+        vm.expectRevert();
+        dsce.mintDsc(AMOUNT_TO_MINT * 1000);
+        vm.stopPrank();
+    }
+
+    function testMintDsc() public depositedCollateral {
+        vm.startPrank(USER);
+        dsce.mintDsc(AMOUNT_COLLATERAL / 10);
+        vm.stopPrank();
+        (uint256 totalDscMinted,) = dsce.getAccountInformation(USER);
+        assertEq(AMOUNT_COLLATERAL / 10, totalDscMinted);
+    }
+
+    ///////////////////
+    // burnDsc Tests //
+    ///////////////////
+    function testRevertWithNothingToBurn() public {
+        vm.prank(USER);
+        vm.expectRevert();
+        dsce.burnDsc(1);
+    }
+
+    function revertIfBurningMoreThanOwned() public depositedCollateralAndMintDsc {
+        vm.startPrank(USER);
+        dsc.approve(address(dsce), AMOUNT_TO_MINT * 1000);
+        vm.expectRevert();
+        dsce.burnDsc(AMOUNT_TO_MINT * 1000);
+        vm.stopPrank();
+    }
+
+    function testBurnDsc() public depositedCollateralAndMintDsc {
+        vm.startPrank(USER);
+        dsc.approve(address(dsce), AMOUNT_TO_MINT);
+        dsce.burnDsc(AMOUNT_TO_MINT);
+        vm.stopPrank();
+        (uint256 totalDscMinted,) = dsce.getAccountInformation(USER);
+        assertEq(0, totalDscMinted);
+    }
+
+    ////////////////////////////
+    // redeemCollateral Tests //
+    ///////////////////////////
+    function testRevertZeroCollateralRedeem() public depositedCollateral {
+        vm.startPrank(USER);
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.redeemCollateral(weth, 0);
+        vm.stopPrank();
+    }
+
+    function testRedeemCollateral() public depositedCollateral {
+        vm.startPrank(USER);
+        dsce.redeemCollateral(weth, AMOUNT_COLLATERAL);
+        vm.stopPrank();
+        (, uint256 collateralValueInUsd) = dsce.getAccountInformation(USER);
+        assertEq(0, collateralValueInUsd);
     }
 }
